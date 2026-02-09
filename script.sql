f2a783aff3a603ff70c3f677f5e028d24e8bfc88
MikajyRz <razafymikajy@gmail.com>
2026-02-09 10:04:53 +0300

    update:script

--- script.sql
+++ script.sql
@@ -7,7 +7,7 @@ CREATE TABLE hotel (
 CREATE TABLE reservation (
     id SERIAL PRIMARY KEY,
 
-    id_client CHAR(4) NOT NULL,  -- 4 chiffres exactement
+    id_client CHAR(4) NOT NULL,  
 
     nombre_passager INTEGER NOT NULL
         CHECK (nombre_passager > 0),
@@ -23,4 +23,29 @@ CREATE TABLE reservation (
         ON DELETE RESTRICT
 );
 
-INSERT INTO hotel(nom) VALUES ('Hotel Test');
+INSERT INTO hotel(nom) VALUES ('Colbert');
+INSERT INTO hotel(nom) VALUES ('Novotel');
+INSERT INTO hotel(nom) VALUES ('Ibis');
+INSERT INTO hotel(nom) VALUES ('Lokanga');
+
+INSERT INTO reservation(id_client, nombre_passager, date_heure_arrive, id_hotel) VALUES
+('4631', 11, '2026-02-05 00:01:00', 3);
+INSERT INTO reservation(id_client, nombre_passager, date_heure_arrive, id_hotel) VALUES
+('4394', 1, '2026-02-05 23:55:00', 3);
+INSERT INTO reservation(id_client, nombre_passager, date_heure_arrive, id_hotel) VALUES
+('8054', 2, '2026-02-09 10:17:00', 1);
+INSERT INTO reservation(id_client, nombre_passager, date_heure_arrive, id_hotel) VALUES
+('1432', 4, '2026-02-01 15:25:00', 2);
+INSERT INTO reservation(id_client, nombre_passager, date_heure_arrive, id_hotel) VALUES
+('7861', 4, '2026-01-28 07:11:00', 1);
+INSERT INTO reservation(id_client, nombre_passager, date_heure_arrive, id_hotel) VALUES
+('3308', 5, '2026-01-28 07:45:00', 1);
+INSERT INTO reservation(id_client, nombre_passager, date_heure_arrive, id_hotel) VALUES
+('4484', 13, '2026-02-28 08:45:00', 2);
+INSERT INTO reservation(id_client, nombre_passager, date_heure_arrive, id_hotel) VALUES
+('9687', 8, '2026-02-28 13:00:00', 2);
+INSERT INTO reservation(id_client, nombre_passager, date_heure_arrive, id_hotel) VALUES
+('6302', 7, '2026-02-15 13:00:00', 1);
+INSERT INTO reservation(id_client, nombre_passager, date_heure_arrive, id_hotel) VALUES
+('8640', 1, '2026-02-18 22:55:00', 4);
+
